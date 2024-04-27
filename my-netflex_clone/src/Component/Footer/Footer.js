940799959fcb42031e62b6c0b562a04017f4fea5
ntadesse01 <hasetmamo01@gmail.com>
2024-04-26 20:49:27 -0700

    my-netflex_clone

---
+++ my-netflex_clone/src/Component/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <>
+    <h1>Hello</h1>
+    </>
+  )
+}
+
+export default Footer
+//rafce
+//Src -> assets -> images
